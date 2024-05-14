061b8771d746edd70e9836ccf330c293b5fb3d66
Minh Tiến Trương <tien.mt36@gmail.com>
2024-05-14 10:31:31 +0700

    namnhuan_t1

--- 145/namnhuan.js
+++ 145/namnhuan.js
@@ -1,5 +1,6 @@
 function nhuan() {
     let x = document.getElementById('nam').value;
+    /*
     if (x % 4 === 0 && x % 100 !== 0) {
         alert('Đây là năm nhuận');
     } else if (x % 100 === 0) {
@@ -8,6 +9,22 @@ function nhuan() {
         } else {
             alert('Không phải năm nhuận');
         }
+    } else {
+        alert('Không phải năm nhuận');
+    }
+}
+     */
+    if (x % 4 === 0) {
+        if (x % 100 === 0) {
+            if (x % 400 === 0) {
+                alert('Đây là năm nhuận');
+            } else {
+                alert('Không phải năm nhuận');
+            }
+        } else {
+            alert('Đây là năm nhuận');
+        }
+
     } else {
         alert('Không phải năm nhuận');
     }
