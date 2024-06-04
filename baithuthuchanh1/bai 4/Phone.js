d08bb5659b7eba8ba2090b49f0167053535436b1
Minh Tiến Trương <tien.mt36@gmail.com>
2024-06-04 10:50:39 +0700

    thuchanh

---
+++ baithuthuchanh1/bai 4/Phone.js
@@ -0,0 +1,42 @@
+class Phone {
+    constructor(code, name, manufacturer, price) {
+        this._code = code;
+        this._name = name;
+        this._manufacturer = manufacturer;
+        this._price = price;
+    }
+
+    get code() {
+        return this._code;
+    }
+    set code(newCode) {
+        this._code = newCode;
+    }
+
+    get name() {
+        return this._name;
+    }
+    set name(newName) {
+        this._name = newName;
+    }
+
+    get manufacturer() {
+        return this._manufacturer;
+    }
+    set manufacturer(newManufacturer) {
+        this._manufacturer = newManufacturer;
+    }
+
+    get price() {
+        return this._price;
+    }
+    set price(newPrice) {
+        this._price = newPrice;
+    }
+
+    toString() {
+        return `Phone: ${this._name}, Code: ${this._code}, Manufacturer: ${this._manufacturer}, Price: ${this._price}`;
+    }
+}
+
+export default Phone;
